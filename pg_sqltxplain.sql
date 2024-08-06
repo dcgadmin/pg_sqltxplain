ac389b56275f7c86271ac56307656f5cda95e800
Deepak Mahto <mahtodeepak05@gmail.com>
2024-08-06 16:32:12 +0530

    added Tooltip for Column with stats

--- pg_sqltxplain.sql
+++ pg_sqltxplain.sql
@@ -32,7 +32,7 @@ select 'pg_sqltxplain' || '_' || abs((:'queryid')::bigint) || '.html' as htmlfil
 \qecho <meta http-equiv="Content-Type" content="text/html; charset=US-ASCII">
 \qecho <meta name="generator" content="PSQL">
 \qecho  <title>PostgreSQL-pg_sqltxplain</title>
-\qecho  <style type='text/css'> .tooltip-container{position:relative;display:inline;cursor:pointer}.tooltip-content{visibility:hidden;position:absolute;left:100%;top:50%;transform:translateY(-50%);background-color:#f9f9f9;border:1px solid #ddd;padding:10px;border-radius:4px;white-space:nowrap;opacity:0;transition:opacity .3s,visibility .3s;z-index:1000}.tooltip-container:hover .tooltip-content{visibility:visible;opacity:1}.tooltip-table{border-collapse:collapse;font-size:.9em}.tooltip-table th,.tooltip-table td{border:1px solid #ddd;padding:4px 8px;text-align:left}.tooltip-table th{background-color:#f2f2f2;font-weight:700} .body {font:11pt Arial,Helvetica,sans-serif; color:black; background:White;} p {font:13pt Arial,Helvetica,sans-serif; color:black; background:White;} table,tr,td {font:12pt Arial,Helvetica,sans-serif; color:Black; background:#f7f7e7; padding:0px 0px 0px 0px; margin:0px 0px 0px 0px;} th {font:bold 10pt Arial,Helvetica,sans-serif; color:#336699; background:#cccc99; padding:0px 0px 0px 0px;} h1 {font:16pt Arial,Helvetica,Geneva,sans-serif; color:#336699; background color:White; border-bottom:1px solid #cccc99; margin-top:0pt; margin-bottom:0pt; padding:0px 0px 0px 0px;- } h2 {font:bold 11pt Arial,Helvetica,Geneva,sans-serif; color:#336699; background-color:White; margin-top:4pt; margin-bottom:0pt;} a {font:9pt Arial,Helvetica,sans-serif; color:#663300; background:#ffffff; margin-top:0pt; margin-bottom:0pt; vertical-align:top;} footer {text-align: right;font-size: smaller;}</style>
+\qecho  <style type='text/css'> .tooltip-container{position:relative;display:inline;cursor:pointer}.tooltip-content{visibility:hidden;position:absolute;left:100%;top:50%;transform:translateY(-50%);background-color:#f9f9f9;border:1px solid #ddd;padding:10px;border-radius:4px;white-space:nowrap;opacity:0;transition:opacity .3s,visibility .3s;z-index:1000}.tooltip-container:hover .tooltip-content{visibility:visible;opacity:1}.tooltip-table{border-collapse:collapse;font-size:.9em}.tooltip-table th,.tooltip-table td{border:1px solid #ddd;padding:4px 8px;text-align:left}.tooltip-table th{background-color:#f2f2f2;font-weight:700} .body {font:11pt Arial,Helvetica,sans-serif; color:black; background:White;} p {font:13pt Arial,Helvetica,sans-serif; color:black; background:White;} table,tr,td {font:12pt Arial,Helvetica,sans-serif; color:Black; background:#f7f7e7; padding:0px 0px 0px 0px; margin:0px 0px 0px 0px;} th {font:bold 10pt Arial,Helvetica,sans-serif; color:#336699; background:#cccc99; padding:0px 0px 0px 0px;} h1 {font:16pt Arial,Helvetica,Geneva,sans-serif; color:#336699; background color:White; border-bottom:1px solid #cccc99; margin-top:0pt; margin-bottom:0pt; padding:0px 0px 0px 0px;- } h2 {font:bold 11pt Arial,Helvetica,Geneva,sans-serif; color:#336699; background-color:White; margin-top:4pt; margin-bottom:0pt;} a {font:9pt Arial,Helvetica,sans-serif; color:#663300; background:#ffffff; margin-top:0pt; margin-bottom:0pt; vertical-align:top;} .xplaina {font:11pt Arial,Helvetica,sans-serif; color:#663300; background:#ffffff; margin-top:0pt; margin-bottom:0pt; vertical-align:top;}  footer {text-align: right;font-size: smaller;}</style>
 \qecho </head>
 \qecho <h1 style="font-family:verdana"align="center">pg_sqltxplain Report - QueryID = :queryid</h1>
 \qecho <div class="table-content">	
@@ -91,12 +91,17 @@ select concat_ws('','<pre>',sql,'</pre>')  as "SQLTEXT" from plan_table where pl
 \qecho <br>
 --select concat_ws('','<pre>',plainplan,'</pre>') as "Execution Plan"  from plan_table where planid = :planid;
 \qecho <pre>
-with plan_table1 as (select (unnest(string_to_array(a.e,' '))) col1, a.r from plan_table ,
-					 lateral unnest(string_to_array(PLAINPLAN,E'\n')) WITH ORDINALITY AS a(e,r) where planid = :planid),
+
+with plan_table as (select * from plan_table where planid = :planid),
+plan_table1 as (select (unnest(string_to_array(a.e,' '))) col1, a.r , starts_with(lower(trim(a.e)),'output:') as addinfo from plan_table ,
+					 lateral unnest(string_to_array(PLAINPLAN,E'\n')) WITH ORDINALITY AS a(e,r) where planid =:planid),
 tblname as (select distinct tblname.* from plan_table , lateral extract_info(jsonplan::jsonb,'Relation Name') as tblname),
-idxname as (select distinct idxname.* from plan_table , lateral extract_info(jsonplan::jsonb,'Index Name') as idxname)
-select string_agg(CASE WHEN exists (SELECT 1 FROM tblname WHERE plan_table1.col1 ~* (tblname.schname || '.' || tblname.objname) LIMIT 1)
-THEN '<div class="tooltip-container"><a href="#Databaseobjects2">' || plan_table1.col1 || '<div class="tooltip-content"><table class="tooltip-table">
+idxname as (select distinct idxname.* from plan_table , lateral extract_info(jsonplan::jsonb,'Index Name') as idxname),
+filters as (select distinct filters.* from plan_table , lateral extract_filters(jsonplan::jsonb)  as filters)
+select string_agg(
+	CASE 
+	WHEN exists (SELECT 1 FROM tblname WHERE plan_table1.col1 ~* (tblname.schname || '.' || tblname.objname) LIMIT 1)
+	THEN '<div class="tooltip-container"><a a class ="xplaina" href="#Databaseobjects2">' || plan_table1.col1 || '<div class="tooltip-content"><table class="tooltip-table">
 				  <tr>
                     <th>Table_Size</th>
                     <th>Index_Size</th>
@@ -120,7 +125,7 @@ tbls."LAnalyze"   as "LAnalyzeTime"
 where tblname.schname like case when tbls."Sname" like 'pg_temp%' then 'pg_temp%' else tbls."Sname" end  and tbls."relname" = tblname.objname) alias1 limit 1)
 				  ||'</table></div></a></div>'
 WHEN exists (SELECT 1 FROM idxname WHERE plan_table1.col1 ~* idxname.objname LIMIT 1)
-THEN '<div class="tooltip-container"><a href="#Databaseobjects3">' || plan_table1.col1 || '<div class="tooltip-content"><table class="tooltip-table">
+THEN '<div class="tooltip-container"><a a class ="xplaina" href="#Databaseobjects3">' || plan_table1.col1 || '<div class="tooltip-content"><table class="tooltip-table">
 				  <tr>
                     <th>IndexSize</th>
                     <th>IndexScan</th>
@@ -142,6 +147,36 @@ idx."Details"      as "IndexDef"
 where idx."Sname" = tblname.schname and idx.relname = tblname.objname
 and idx.indexrelname = idxname.objname and plan_table1.col1 ~* idxname.objname ) alias1 limit 1)
 				  ||'</table></div></a></div>'
+	WHEN not addinfo and exists (SELECT 1 FROM filters WHERE strpos(objname,plan_table1.col1) > 0 LIMIT 1)
+THEN coalesce('<div class="tooltip-container"><a class ="xplaina" href="#Databaseobjects4">' || plan_table1.col1 || '<div class="tooltip-content"><table class="tooltip-table">
+				  <tr>
+				  	<th>TableName</th>
+				  	<th>ColumnName</th>
+                    <th>DataType</th>
+                    <th>Nullable</th>
+				    <th>Null Fraction</th>
+				    <th>Distinct</th>
+				    <th>Correlation</th>
+				  	<th>Selectivity</th>
+					<th>Storage Type</th>
+					<th>Statistics Target</th>
+                </tr>' ||
+				  (select concat_ws('','<tr><td>',"TableName",'</td><td>',"ColumnName",'</td><td>',"DataType",'</td><td>',"Nullable",'</td><td>',"Null Fraction",
+									'</td><td>',"Distinct",'</td><td>',"Correlation",
+				 '</td><td>',"Selectivity",'</td><td>',"Storage Type",'</td><td>',"Statistics Target",'</td></tr>')
+from 
+(SELECT  cols."TName" as "TableName" , cols."CName" as "ColumnName" , cols."Type"       as "DataType",
+cols."NULL?"      as "Nullable",
+cols."Null%"      as "Null Fraction",
+cols."Distnct"    as "Distinct",
+cols."Cluster"    as "Correlation",
+cols."Selectivity" as "Selectivity",
+cols."Store"      as "Storage Type",
+cols."StatTarget" as "Statistics Target"
+ from planstats.VW_COLUMN_STATS cols , tblname 
+	where cols."SName" = tblname.schname and cols."TName" = tblname.objname
+and  strpos(plan_table1.col1,cols."CName") > 0 limit 1) alias1 limit 1)
+				  ||'</table></div></a></div>',plan_table1.col1)
 else plan_table1.col1
 end,' ')
 from plan_table1
@@ -184,9 +219,7 @@ ORDER BY total_exec_time DESC;
 \qecho <h4>This section shows the underlying stats of the table referenced in the execution plan.</h4>
 
 with plan_table as (select * from plan_table where planid = :planid), 
-tblname as (select distinct tblname.* from plan_table , lateral extract_info(jsonplan::jsonb,'Relation Name') as tblname),
-idxname as (select distinct idxname.* from plan_table , lateral extract_info(jsonplan::jsonb,'Index Name') as idxname),
-filters as (select distinct filters.* from plan_table , lateral extract_filters(jsonplan::jsonb)  as filters)
+tblname as (select distinct tblname.* from plan_table , lateral extract_info(jsonplan::jsonb,'Relation Name') as tblname)
 select distinct tbls."Sname"      as "SchemaName",
 tbls.relname    as "TableName",
 pg_size_pretty(pg_relation_size(relname::regclass)) as "Table_Size",
@@ -215,8 +248,7 @@ where tblname.schname like case when tbls."Sname" like 'pg_temp%' then 'pg_temp%
 
 with plan_table as (select * from plan_table where planid = :planid), 
 tblname as (select distinct tblname.* from plan_table , lateral extract_info(jsonplan::jsonb,'Relation Name') as tblname),
-idxname as (select distinct idxname.* from plan_table , lateral extract_info(jsonplan::jsonb,'Index Name') as idxname),
-filters as (select distinct filters.* from plan_table , lateral extract_filters(jsonplan::jsonb)  as filters)
+idxname as (select distinct idxname.* from plan_table , lateral extract_info(jsonplan::jsonb,'Index Name') as idxname)
 select distinct idx."Sname"        as "SchemaName",
 idx.relname      as "TableName",
 idx.indexrelname as "IndexName",
